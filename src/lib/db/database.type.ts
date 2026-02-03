8ecede041463c06447a8dc36814b9181fd6f88cd
szknuts <xeno4absolute@gmail.com>
2026-02-03 13:57:48 +0900

    add: バックエンド 請求作成機能

--- src/lib/db/database.type.ts
+++ src/lib/db/database.type.ts
@@ -25,3 +25,24 @@ export interface Transfer {
   message?: string; // メッセージ（オプショナル）
   created_at: string; // 作成日時
 }
+
+/**
+ * @description payment_requestsテーブルの状態
+ * - pending: 待機中
+ * - accepted: 承認
+ * - rejected: 却下
+ */
+export type PaymentRequestState = "pending" | "accepted" | "rejected";
+
+/**
+ * @description payment_requestsテーブルの型定義
+ */
+export interface PaymentRequest {
+  id: string; // 支払い依頼ID
+  requester_id: string; // 請求したユーザーID
+  payer_id: string | null; // 支払うユーザーID（まだ誰も決まっていない場合はnull）
+  amount: number; // 送金額
+  message?: string; // メッセージ（オプショナル）
+  created_at: string; // 作成日時
+  state: PaymentRequestState; // 状態
+}
